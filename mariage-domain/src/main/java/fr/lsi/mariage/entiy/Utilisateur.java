86629e59c3593e7ac7ae920793410a2172c04557
Laurent <lau.sion@gmail.com>
2019-12-08 11:25:29 +0100

    Retrait des finals pour les getters hibernate

--- mariage-domain/src/main/java/fr/lsi/mariage/entiy/Utilisateur.java
+++ mariage-domain/src/main/java/fr/lsi/mariage/entiy/Utilisateur.java
@@ -56,84 +56,84 @@ public class Utilisateur {
 	/**
 	 * @return the uuid
 	 */
-	public final String getUuid() {
+	public String getUuid() {
 		return uuid;
 	}
 
 	/**
 	 * @param uuid the uuid to set
 	 */
-	public final void setUuid(String uuid) {
+	public void setUuid(String uuid) {
 		this.uuid = uuid;
 	}
 
 	/**
 	 * @return the login
 	 */
-	public final String getLogin() {
+	public String getLogin() {
 		return login;
 	}
 
 	/**
 	 * @param login the login to set
 	 */
-	public final void setLogin(String login) {
+	public void setLogin(String login) {
 		this.login = login;
 	}
 
 	/**
 	 * @return the password
 	 */
-	public final String getPassword() {
+	public String getPassword() {
 		return password;
 	}
 
 	/**
 	 * @param password the password to set
 	 */
-	public final void setPassword(String password) {
+	public void setPassword(String password) {
 		this.password = password;
 	}
 
 	/**
 	 * @return the dateCreation
 	 */
-	public final LocalDate getDateCreation() {
+	public LocalDate getDateCreation() {
 		return dateCreation;
 	}
 
 	/**
 	 * @param dateCreation the dateCreation to set
 	 */
-	public final void setDateCreation(LocalDate dateCreation) {
+	public void setDateCreation(LocalDate dateCreation) {
 		this.dateCreation = dateCreation;
 	}
 
 	/**
 	 * @return the dateModification
 	 */
-	public final LocalDate getDateModification() {
+	public LocalDate getDateModification() {
 		return dateModification;
 	}
 
 	/**
 	 * @param dateModification the dateModification to set
 	 */
-	public final void setDateModification(LocalDate dateModification) {
+	public void setDateModification(LocalDate dateModification) {
 		this.dateModification = dateModification;
 	}
 
 	/**
 	 * @return the dateDerniereConnexion
 	 */
-	public final LocalDate getDateDerniereConnexion() {
+	public LocalDate getDateDerniereConnexion() {
 		return dateDerniereConnexion;
 	}
 
 	/**
 	 * @param dateDerniereConnexion the dateDerniereConnexion to set
 	 */
-	public final void setDateDerniereConnexion(LocalDate dateDerniereConnexion) {
+	public void setDateDerniereConnexion(LocalDate dateDerniereConnexion) {
 		this.dateDerniereConnexion = dateDerniereConnexion;
 	}
 
